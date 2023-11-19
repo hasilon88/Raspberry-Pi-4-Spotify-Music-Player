c2e49f9f2780da29eb2c24dc56a13c7c4c34c7a7
Harjot Singh <109122423+hasilon88@users.noreply.github.com>
2023-11-19 01:54:26 -0500

    Add files via upload

---
+++ spotify_controls.py
@@ -0,0 +1,136 @@
+import base64
+import requests
+
+CLIENT_ID = 'e7475af827eb493eb04d608eb62dc6b1'
+CLIENT_SECRET = 'f767a453e9f04fd2adfd42577cee6d0d'
+REDIRECT_URI = 'https://eob5j15ki6at15.m.pipedream.net'
+
+# RequestBin
+USER_TOKEN = 'BQC8IRtGnmiYJD2_zQCWmmX12G2RYC9qpqkediDVGfPmfgbH_hBfn_WE9oulmDOnUxAfzGz3-8Izq2xht6GoTfgMhCP15_weJTez_hBgnZeEfQIAleFfbGnz2g-Q2Zj01X8AEkGwMBJbvrxtZ19kqnUc1B4kHtJoauOiQjIyZZRGvbejixY2XrRNI1r1-utEj8subvT-CSQPRFLqV5c82a43UfoIhoQ7NLJjnDx2__GydBoYwl0Kzv1lDtvTkmpbAIIUWD7nsiQAYVbvJG2Nu0OLimqTmh3yspH5LT_71ZQl9ODhlICh'
+
+# --- Setup ---
+
+def getUserCode():
+    SCOPE = 'user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private user-library-read user-library-modify user-top-read user-read-recently-played user-follow-read user-follow-modify user-read-private user-read-email user-library-read user-library-modify'
+    SCOPE = SCOPE.replace(" ", "+")
+
+    authorize_url = 'https://accounts.spotify.com/authorize'
+    params = {
+        'client_id': CLIENT_ID,
+        'response_type': 'code',
+        'redirect_uri': REDIRECT_URI,
+        'scope': SCOPE
+    }
+
+    authorization_url = f"{authorize_url}?{'&'.join([f'{key}={value}' for key, value in params.items()])}"
+
+    print(f"Please visit the following URL to authorize your application:\n{authorization_url}")
+
+def convertCodeToToken(AUTHORIZATION_CODE):
+    authorization_header = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
+
+    token_url = 'https://accounts.spotify.com/api/token'
+    token_data = {
+        'grant_type': 'authorization_code',
+        'code': AUTHORIZATION_CODE,
+        'redirect_uri': REDIRECT_URI,
+    }
+
+    headers = {
+        'Authorization': f'Basic {authorization_header}',
+    }
+
+    response = requests.post(token_url, data=token_data, headers=headers)
+
+    if response.status_code == 200:
+        token_info = response.json()
+        access_token = token_info.get('access_token')
+        refresh_token = token_info.get('refresh_token')
+        token_type = token_info.get('token_type')
+        expires_in = token_info.get('expires_in')
+        
+        print(f"Access Token: {access_token}")
+        print(f"Refresh Token: {refresh_token}")
+        print(f"Token Type: {token_type}")
+        print(f"Expires In: {expires_in} seconds")
+    else:
+        print("Failed to obtain access token.")
+    
+    return access_token
+
+# --------------
+
+def getCurrentPlayingSong():
+    current_track_url = 'https://api.spotify.com/v1/me/player/currently-playing'
+
+    headers = {
+        'Authorization': f'Bearer {USER_TOKEN}',
+    }
+
+    response = requests.get(current_track_url, headers=headers)
+
+    if response.status_code == 200:
+        current_track_data = response.json()
+
+        return {
+            'track_name': current_track_data.get('item', {}).get('name'),
+            'artist_names': [artist['name'] for artist in current_track_data.get('item', {}).get('artists', [])],
+            'album_name': current_track_data.get('item', {}).get('album', {}).get('name')
+        }
+    
+    else:
+        print("Failed to fetch currently playing track data.\nError code: ", response.status_code)
+
+def togglePlay():
+    current_playback_url = 'https://api.spotify.com/v1/me/player'
+    
+    headers = {
+        'Authorization': f'Bearer {USER_TOKEN}',
+    }
+    
+    response = requests.get(current_playback_url, headers=headers)
+
+    if response.status_code == 200:
+        playback_data = response.json()
+
+        is_playing = playback_data.get('is_playing')
+
+        if is_playing:
+            action_url = 'https://api.spotify.com/v1/me/player/pause'
+        else:
+            action_url = 'https://api.spotify.com/v1/me/player/play'
+        
+        action_response = requests.put(action_url, headers=headers)
+        
+        if action_response.status_code != 204:
+            print("Failed to toggle pause/play.")    
+
+    else:
+        print("Failed to fetch current playback data.")
+
+def nextSong():
+    next_track_url = 'https://api.spotify.com/v1/me/player/next'
+
+    headers = {
+        'Authorization': f'Bearer {USER_TOKEN}',
+    }
+
+    response = requests.post(next_track_url, headers=headers)
+
+    if response.status_code != 204:
+        print("Failed to skip to the next track.")
+
+def previousSong():
+    next_track_url = 'https://api.spotify.com/v1/me/player/previous'
+
+    headers = {
+        'Authorization': f'Bearer {USER_TOKEN}',
+    }
+
+    response = requests.post(next_track_url, headers=headers)
+
+    if response.status_code != 204:
+        print("Failed to skip to the next track.")
+
+previousSong()
+
